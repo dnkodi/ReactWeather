ec112485ae6203c1834ae205b2b7758f2c7f49e0
dulip <dk@yahoo.com>
2017-02-14 09:03:22 +0530

    Added error modal

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -9,14 +9,16 @@ module.exports = {
     var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
     return axios.get(requestUrl).then(function(res){
-      
+
       if (res.data.cod && res.data.message){
-        throw new Error (res.data.message);
+        throw new Error(res.data.message);
+        //console.log(res.data.cod, res.data.message);
       }else{
         return res.data.main.temp;
       }
-    }, function(res){
-      throw new Error(res.data.message);
+    }, function(err){
+      throw new Error(err.response.data.message);
+      //console.log(res.data.message);
     })
   }
 }
